97b9139d8e5ea98b5f1ba4f20444c8097ab2cac7
michaelhuynh4000 <michaelhuynh4000@gmail.com>
2017-09-08 10:25:17 +1000

    first commit

---
+++ archive.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import time
+import sys
+import subprocess
+import os
+
+#get filepath arguments separated by spaces
+files = ' '.join(sys.argv[1:])
+
+today = time.strftime("%Y-%b-%d").upper()
+
+#get archive name
+archiveName = today + ".tar.gz"
+
+def archive():
+	#run bash command
+	#https://stackoverflow.com/questions/4256107/running-bash-commands-in-python
+	bashCommand = "tar czvf " + archiveName + " " + files
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
+
+	print("\"{}\" was archived!".format(files));
+
+#if no archive exists create one
+if(not os.path.exists(archiveName)):
+	archive()
+
+#if archive not exist ask if want to overwrite? else cancel.
+else:
+	print("Archive named \"{}\" already exists!".format(archiveName))
+	overwrite = input("Do you wish to overwrite (y/n)? ")
+	if(overwrite == "y"):
+		archive()
+	else:
+		print("Backup aborted!");
+
